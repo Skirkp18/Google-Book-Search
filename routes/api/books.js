ef53c49befbb6af52a1fbf29d3f1a03b2f80e501
Sean Kirkpatrick <Skirkp18@gmail.com>
2021-01-11 18:44:16 -0500

    added suedo code for books.js page

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const bookController = require("../../controllers/bookController");
 
-
+// routes for when just /books/ is hit
 router.route("/")
   .get(bookController.findAll)
   .post(bookController.create);
 
-
+// routes for when /books/id is hit
 router
   .route("/:id")
   .get(bookController.findById)
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-
+// use /api then routes in page
 router.use("/api", apiRoutes);
 
-
+// create the router
 router.use((req, res) =>
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
 );
